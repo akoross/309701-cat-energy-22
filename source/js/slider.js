ce72f63d09379e2c35e40e0746789c140e2b1593
Sergii <74363342+akoross@users.noreply.github.com>
2021-05-08 19:32:16 +0300

    slider js index.html

---
+++ source/js/slider.js
@@ -0,0 +1,20 @@
+const rangeSlider = document.querySelector(".slider-toggles__bar");
+const sliderImage = document.querySelector(".slider__item--before");
+const buttonBefore = document.querySelector(".slider-toggles__button--before");
+const buttonAfter = document.querySelector(".slider-toggles__button--after");
+
+sliderImage.style.width = rangeSlider.value + "%";
+
+rangeSlider.oninput = function () {
+  sliderImage.style.width = this.value + "%";
+};
+
+buttonBefore.addEventListener("click", () => {
+  rangeSlider.value = 0;
+  sliderImage.style.width = 100 + "%";
+});
+
+buttonAfter.addEventListener("click", () => {
+  rangeSlider.value = 100;
+  sliderImage.style.width = 0;
+});
